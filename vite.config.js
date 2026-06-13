cb8741e818332787305b1a575c8ca2e86e995e82
Khushi Sharma <ks0541929@gmail.com>
2026-06-13 16:13:19 +0530

    updated manifest.json

--- vite.config.js
+++ vite.config.js
@@ -12,60 +12,60 @@ export default defineConfig({
         name: "TrackFit",
         short_name: "Fit",
         description: 'Your offline-first workout partner.',
-        theme_color: '#000000',
-        background_color: '#1A1A1A',
+        theme_color: '#20201F',
+        background_color: '#20201F',
         "icons": [
-          {
-            "src": "icons/fit_icon-48x48.png",
-            "sizes": "48x48",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-128x128.png",
-            "sizes": "128x128",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-152x152.png",
-            "sizes": "152x152",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-256x256.png",
-            "sizes": "256x256",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/fit_icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
-        ],
+    {
+      "src": "icons/Fit-48x48.png",
+      "sizes": "48x48",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-72x72.png",
+      "sizes": "72x72",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-96x96.png",
+      "sizes": "96x96",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-128x128.png",
+      "sizes": "128x128",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-144x144.png",
+      "sizes": "144x144",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-152x152.png",
+      "sizes": "152x152",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-256x256.png",
+      "sizes": "256x256",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-384x384.png",
+      "sizes": "384x384",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Fit-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
         start_url: '.',
        
         display: "standalone"
